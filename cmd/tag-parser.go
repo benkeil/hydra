e8374c1720612c59cce3eb5b0344839cde1a03dd
Ben Keil <benkeil.me@googlemail.com>
2018-03-10 11:34:38 +0100

    hydra now supports „not semver“ tags for development builds

--- cmd/tag-parser.go
+++ cmd/tag-parser.go
@@ -9,24 +9,18 @@ import (
 
 const semverTag string = "semver"
 
-// DefaultTagParser reads the hydra config file
-type DefaultTagParser struct {
-	semverVersion semver.Version
-}
-
 // TagParser is the interface for the Parser
 type TagParser interface {
 	parseTags(version Version) []string
-	parseTag(tag string) []string
 }
 
-// NewParser creates an new Parser
-func NewParser(version string) DefaultTagParser {
-	return DefaultTagParser{semver.MustParse(version)}
+// SemverTagParser parse semver based version
+type SemverTagParser struct {
+	semverVersion semver.Version
 }
 
 // parseTags parse all configured tags
-func (parser *DefaultTagParser) parseTags(version Version) []string {
+func (parser *SemverTagParser) parseTags(version Version) []string {
 	var parsedTags []string
 	for _, tag := range version.Tags {
 		logger.Debugf("Parse tag: %v\n", tag)
@@ -34,14 +28,14 @@ func (parser *DefaultTagParser) parseTags(version Version) []string {
 	}
 	if len(version.Tags) == 0 {
 		logger.Debugf("Using DefaultStrategy for directory: %s", version.Directory)
-		strategy := DefaultStrategy{version, parser.semverVersion}
+		strategy := DefaultStrategy{version, parser.semverVersion.String()}
 		parsedTags = append(parsedTags, strategy.GetTags()...)
 	}
 	return parsedTags
 }
 
 // parseTag parse just one tag and returns all corresponding tags
-func (parser *DefaultTagParser) parseTag(version Version, tag string) []string {
+func (parser *SemverTagParser) parseTag(version Version, tag string) []string {
 	var parsedTags []string
 	if strings.Contains(tag, semverTag) {
 		logger.Debugf("Using SemverStrategy for tag: %v", tag)
@@ -56,6 +50,60 @@ func (parser *DefaultTagParser) parseTag(version Version, tag string) []string {
 	return parsedTags
 }
 
+// DefaultTagParser parse not semver based versions
+type DefaultTagParser struct {
+	version string
+}
+
+// parseTags parse all configured tags
+func (parser *DefaultTagParser) parseTags(version Version) []string {
+	var parsedTags []string
+	for _, tag := range version.Tags {
+		logger.Debugf("Parse tag: %v\n", tag)
+		parsedTags = append(parsedTags, parser.parseTag(version, tag)...)
+	}
+	if len(version.Tags) == 0 {
+		logger.Debugf("Using DefaultStrategy for directory: %s", version.Directory)
+		strategy := DefaultStrategy{version, parser.version}
+		parsedTags = append(parsedTags, strategy.GetTags()...)
+	}
+	return parsedTags
+}
+
+// parseTag parse just one tag and returns all corresponding tags
+func (parser *DefaultTagParser) parseTag(version Version, tag string) []string {
+	var parsedTags []string
+	if strings.Contains(tag, semverTag) {
+		logger.Debugf("Using ReplaceStrategy for tag: %v", tag)
+		strategy := ReplaceStrategy{tag, parser.version}
+		parsedTags = append(parsedTags, strategy.GetTags()...)
+	}
+	logger.Debugf("Parsed tags: %v\n", parsedTags)
+	return parsedTags
+}
+
+// TagParserFactory is a factory for a TagParser
+type TagParserFactory func(version string) TagParser
+
+// NewDefaultTagParser creates a new DefaultTagParser
+func NewDefaultTagParser(version string) TagParser {
+	return &DefaultTagParser{version: version}
+}
+
+// NewSemverTagParser creates a new SemverTagParser
+func NewSemverTagParser(version semver.Version) TagParser {
+	return &SemverTagParser{semverVersion: version}
+}
+
+// NewParser creates an new Parser
+func NewParser(version string) TagParser {
+	v, err := semver.Parse(version)
+	if err != nil {
+		return NewDefaultTagParser(version)
+	}
+	return NewSemverTagParser(v)
+}
+
 // TagStrategy is the interface for all tagging strategies
 type TagStrategy interface {
 	GetTags() []string
@@ -63,13 +111,13 @@ type TagStrategy interface {
 
 // DefaultStrategy returns the default tag for an image
 type DefaultStrategy struct {
-	Version       Version
-	semverVersion semver.Version
+	Version Version
+	version string
 }
 
 // GetTags returns the default tags for an image
 func (strategy *DefaultStrategy) GetTags() []string {
-	return []string{fmt.Sprintf("%s-%s", strategy.semverVersion, strings.Replace(strategy.Version.Directory, "/", "-", -1))}
+	return []string{fmt.Sprintf("%s-%s", strategy.version, strings.Replace(strategy.Version.Directory, "/", "-", -1))}
 }
 
 // SemverStrategy if a tags contains the string "semver", this strategy splits it into three convenient tags
@@ -96,4 +144,17 @@ type SimpleStrategy struct {
 // GetTags returns a simple tag like "latest" for an image
 func (strategy *SimpleStrategy) GetTags() []string {
 	return []string{strategy.Tag}
+}
+
+// ReplaceStrategy just returns the tag defined in the config
+type ReplaceStrategy struct {
+	Tag     string
+	Version string
+}
+
+// GetTags returns a simple tag like "latest" for an image
+func (strategy *ReplaceStrategy) GetTags() []string {
+	return []string{
+		strings.Replace(strategy.Tag, semverTag, fmt.Sprintf("%s", strategy.Version), -1),
+	}
 }
