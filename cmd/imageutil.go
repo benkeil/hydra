4017aafe710126bd2725db4b14dd80c8b2e94723
Leonid Fot <l.fot@leonid-fot.info>
2019-02-01 21:09:23 +0100

    add initial support for docker auth

--- cmd/imageutil.go
+++ cmd/imageutil.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/docker/cli/cli/config/types"
 )
+
+type authHeader struct {
+	Username      string `json:"username"`
+	Password      string `json:"password"`
+	ServerAddress string `json:"serveraddress"`
+}
 
 // ImageUtil provides some helper methods
 type ImageUtil interface {
 	getImageTags(config Config, tags []string) []string
+	getRegistryHostnames(config Config) []string
+	encodeAuth(authConfig *types.AuthConfig) string
 }
 
 // DefaultImageUtil is the default implementation for the ImageUtil interface
@@ -29,4 +42,33 @@ func (helper *DefaultImageUtil) getImageTags(config Config, tags []string) []str
 		}
 	}
 	return imageTags
+}
+
+// getRegistryHostnames returns the registry hostname:port component of image name
+func (helper *DefaultImageUtil) getRegistryHostnames(config Config) []string {
+
+	images := []string{}
+	for _, image := range config.Image {
+		images = append(images, strings.Split(image, "/")[0])
+	}
+
+	return images
+}
+
+// encodeAuth returns the base64 endoded JSON object for AuthConfig struct
+func (helper *DefaultImageUtil) encodeAuth(authConfig *types.AuthConfig) string {
+	if authConfig.Username == "" && authConfig.Password == "" {
+		return ""
+	}
+
+	auth := &authHeader{
+		Username:      authConfig.Username,
+		Password:      authConfig.Password,
+		ServerAddress: authConfig.ServerAddress,
+	}
+	authStr, _ := json.Marshal(auth)
+	msg := []byte(authStr)
+	encoded := make([]byte, base64.StdEncoding.EncodedLen(len(msg)))
+	base64.StdEncoding.Encode(encoded, msg)
+	return string(encoded)
 }
